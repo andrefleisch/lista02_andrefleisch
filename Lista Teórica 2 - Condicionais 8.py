c71e058b2a4818f80e49ba5146d04fe031620e9b
andrefleisch <andrefleisch@hotmail.com>
2024-03-20 21:34:38 -0300

    Create Lista Teórica 2 - Condicionais 8.py

---
+++ Lista Teórica 2 - Condicionais 8.py
@@ -0,0 +1,7 @@
+idade = int(input("Digite a sua idade: "))
+if idade < 16:
+    print("Não eleitor")
+elif idade >= 16 and idade < 18 or idade > 65:
+    print("Eleitor facultativo")    
+elif idade >= 18 and idade <= 65:
+    print("Eleitor obrigatório")
