ecf466b903b4f6a0dbd0af959407433f71f018d3
andrefleisch <andrefleisch@hotmail.com>
2024-03-20 21:34:21 -0300

    Create Lista Teórica 2 - Condicionais 3.py

---
+++ Lista Teórica 2 - Condicionais 3.py
@@ -0,0 +1,7 @@
+temperatura = float(input("Digite a temperatura do dia de hoje em celsius: "))
+if temperatura < 15:
+    print("Está frio!")
+elif temperatura > 25:
+    print("Está calor!")    
+else:
+    print("Temperatura agradável!")    
