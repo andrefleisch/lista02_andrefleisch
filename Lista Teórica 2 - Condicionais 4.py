f878085e2de95da371c02f221d43562b7fc37ec2
andrefleisch <andrefleisch@hotmail.com>
2024-03-20 21:34:24 -0300

    Create Lista Teórica 2 - Condicionais 4.py

---
+++ Lista Teórica 2 - Condicionais 4.py
@@ -0,0 +1,7 @@
+nota = float(input("Digite a sua nota: "))
+if nota >= 7:
+    print("Estudante aprovado!")
+elif nota >= 4 < 7:
+    print("Estudante em recuperação")    
+elif nota < 4:
+    print("Estudante reprovado")    
